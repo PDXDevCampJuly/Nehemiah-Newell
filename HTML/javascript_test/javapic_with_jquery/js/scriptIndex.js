3b79c2bd07c4ef8310edff280e0b196511aa823e
nehemiahnewell <nehemiahnewell@gmail.com>
2015-09-01 15:54:42 -0700

    The Javapics pages, now with jquery
    The javascript has been rewritten to use jquery selectors.

---
+++ HTML/javascript_test/javapic_with_jquery/js/scriptIndex.js
@@ -0,0 +1,37 @@
+/**
+* Created by Nehemiah on 8/26/2015.
+*/
+
+var $jumbotron = $('#jumbotron');
+var listOfImages = [];
+var currentImage = 56;
+
+(function () {
+    var i;
+    for(i = 1; i < 10;i++)
+    {
+        var num = i.toString();
+        listOfImages.push("'images/pdxcg_0" + num + ".jpg'");
+    }
+    for(; i <= 60;i++)
+    {
+        if(i === 36 || i === 42){i++;} //These two photoes are missing - comment out this line if they're found.
+        var num = i.toString();
+        listOfImages.push("'images/pdxcg_" + num + ".jpg'");
+    }
+}());
+
+function changeTheImage()
+{
+    currentImage++;
+    if(currentImage === 58)
+	{
+		currentImage = 0;
+	}
+	$jumbotron.css('background-image', 'url(' + listOfImages[currentImage] + ')');
+    console.log(currentImage)
+    setTimeout(changeTheImage, 20000);
+
+}
+
+changeTheImage();
